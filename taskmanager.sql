902aac4a18aeb09314812be6148d19dd36cc5d55
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-03-18 17:31:45 -0700

    changed sql

--- taskmanager.sql
+++ taskmanager.sql
@@ -1,15 +1,13 @@
 CREATE DATABASE taskmanager;
 USE taskmanager;
 
-CREATE TABLE task(
+CREATE TABLE item(
 	id INT auto_increment,
     fname VARCHAR(255),
     lname VARCHAR(255),
-    task VARCHAR(255),
     description VARCHAR(255),
-    startdate date,
-    enddate date,
-    tasktime time,
+    assignedday VARCHAR(255),
+    days INT,
     urgency VARCHAR(255),
     created_at DATETIME DEFAULT NOW(),
     
